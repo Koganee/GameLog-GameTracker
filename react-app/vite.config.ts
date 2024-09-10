b06fa6edc99793826bf0e756e6b1c02712605755
Aaron Murphy <126209301+Koganee@users.noreply.github.com>
2024-09-10 22:09:48 +0100

    Update vite.config.ts

--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/react-app/',
+  build: {
+    outDir: 'dist',
+  },
   server: {
     port: 5173, // Specify your desired port here
   },
